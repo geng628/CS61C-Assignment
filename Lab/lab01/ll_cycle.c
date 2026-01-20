378d2165a814774e76888ae7e05c02e1bb0df1e3
geng628 <3117067650qq.com>
2026-01-20 11:27:46 +0800

    lab01 solution

--- Lab/lab01/ll_cycle.c
+++ Lab/lab01/ll_cycle.c
@@ -3,5 +3,15 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *slow = head;
+    node *fast = head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast){
+            return 1;
+        }
+    }
     return 0;
 }
+// 因为fast是走两步，slow是走一步，其实相对于slow来说，fast是一个节点一个节点的靠近slow的，所以fast一定可以和slow重合
--- Lab/lab01/no_segfault_ex.c
+++ Lab/lab01/no_segfault_ex.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main() {
     int a[5] = {1, 2, 3, 4, 5};
-    unsigned total = 0;
-    for (int j = 0; j < sizeof(a); j++) {
+    int total = 0;
+    for (int j = 0; j < sizeof(a)/sizeof(int); j++) {
         total += a[j];
     }
     printf("sum of array is %d\n", total);
 }
+//sizeof()里面是计算字节的多少的，这里里面数组为40个字节，没有发生段错误也仅仅是侥幸
