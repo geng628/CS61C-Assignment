f2c36080e7beafc6b20a7b1ce7792526aee76127
geng628 <3117067650qq.com>
2026-01-27 23:57:39 +0800

    project2 B1

--- Project/proj2/src/read_matrix.s
+++ Project/proj2/src/read_matrix.s
@@ -26,17 +26,88 @@
 # ==============================================================================
 read_matrix:
 
+    addi sp,sp, -20
+    sw s0, 0(sp)
+    sw s1, 4(sp)
+    sw s2, 8(sp)
+    sw s3, 12(sp)
+    sw ra, 16(sp)
+
+    mv s1, a1
+    mv s2, a2
     # Prologue
-	
+    
 
+    mv a1, a0
+    li a2, 0    #only read
+    jal fopen   #return a0, a0 is file descriptor
+    mv s0, a0
+    
 
+    li a0, 8
+    jal malloc
+    
+    mv t0, a0
+    mv a1, s0
+    mv a2, t0
+    li a3, 8
+    addi sp,sp -12
+    sw a0, 4(sp)
+    sw a1, 8(sp)
+    sw a2, 0(sp)
+    jal fread
+    
+    lw a2, 0(sp)
+    lw a1, 8(sp)
+    lw a0, 4(sp)
+    addi sp,sp, 12
 
+    lw t1,0(a2)
+    lw t2,4(a2)
+    sw t1,0(s1)  # row
+    sw t2,0(s2)  # column
 
+    mul t1, t1, t2
+    slli t1, t1, 2
+    mv a0, t1
+    addi sp,sp, -4
+    sw t1, 0(sp)
+    jal malloc
+    
+    lw t1, 0(sp)
+    addi sp,sp, 4
+    mv a1, s0
+    mv s3, a0
+    mv a2, s3
+    mv a3, t1
+    
+    jal fread
+    
 
-
-
-
+    mv a1, s0
+    jal fclose
+    
+    mv a0, s3
     # Epilogue
-
-
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    lw s3, 12(sp)
+    lw ra, 16(sp)
+    addi sp,sp, 20
     ret
+
+fclose_error:
+    li a1, 92
+    j fal_error
+fread_error:
+    li a1, 91
+    j fal_error
+fopen_error:
+    li a1, 90
+    j fal_error
+malloc_error:
+    li a1, 88
+    j fal_error
+fal_error:
+    jal exit2
